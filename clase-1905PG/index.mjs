4301d00b82234bed46cd804df30497954f716f12
erikavaninatello <vaninatello9@gmail.com>
2026-05-20 23:19:04 -0300

    importante2

---
+++ clase-1905PG/index.mjs
@@ -0,0 +1,17 @@
+
+import express from 'express'
+import rutasProductos from './modulos/productos/rutas.productos.mjs'
+
+const PUERTO = 3000
+
+const app = express()
+// /api/v1
+// /v1
+// /
+
+app.use(rutasProductos)
+
+
+
+
+app.listen(PUERTO)
